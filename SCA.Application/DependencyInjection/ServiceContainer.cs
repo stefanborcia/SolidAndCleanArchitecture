37c688306e95be4ea43c895515a68d81a1a7dfcf
stefanborcia <stefigabi89@yahoo.com>
2025-04-24 20:42:09 +0200

    Creating Dependency Injection Containers.

---
+++ SCA.Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SCA.Application.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            // Add your infrastructure services here
+
+            return services;
+        }
+    }
+}
---
+++ SCA.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SCA.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services)
+        {
+            // Add your infrastructure services here
+
+            return services;
+        }
+    }
+}
