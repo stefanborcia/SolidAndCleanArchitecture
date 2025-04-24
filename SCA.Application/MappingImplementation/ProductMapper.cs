943c4ed74535cb018f1ddf2ebda3872b4e32807f
stefanborcia <stefigabi89@yahoo.com>
2025-04-24 21:07:57 +0200

    Implement Mapping Interface and Implementation

---
+++ SCA.Application/MappingImplementation/ProductMapper.cs
@@ -0,0 +1,39 @@
+﻿using SCA.Application.MappingInterface;
+using SCA.Application.ProductDTOs;
+using SCA.Domain.ProductEntity;
+
+namespace SCA.Application.MappingImplementation
+{
+    internal class ProductMapper : IProductMapper
+    {
+        public ProductDto MapToDto(Product product)
+        {
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock
+            };
+        }
+        public Product MapToEntity(CreateProductDto createProductDto)
+        {
+            return new Product
+            {
+                Name = createProductDto.Name,
+                Price = createProductDto.Price,
+                Stock = createProductDto.Stock
+            };
+        }
+        public Product MapToEntity(UpdateProductDto updateProductDto)
+        {
+            return new Product
+            {
+                Id = updateProductDto.Id,
+                Name = updateProductDto.Name,
+                Price = updateProductDto.Price,
+                Stock = updateProductDto.Stock
+            };
+        }
+    }
+}
---
+++ SCA.Application/MappingInterface/IProductMapper.cs
@@ -0,0 +1,14 @@
+﻿
+
+using SCA.Application.ProductDTOs;
+using SCA.Domain.ProductEntity;
+
+namespace SCA.Application.MappingInterface
+{
+    public interface IProductMapper
+    {
+        ProductDto MapToDto(Product product);
+        Product MapToEntity(CreateProductDto createProductDto);
+        Product MapToEntity(UpdateProductDto updateProductDto);
+    }
+}
