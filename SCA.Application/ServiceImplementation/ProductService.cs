e0bf80d9803beec36534323e2f40d10f57569ae1
stefanborcia <stefigabi89@yahoo.com>
2025-04-24 21:20:36 +0200

    Implement Service Interface and implementation for Product

---
+++ SCA.Application/ServiceImplementation/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using SCA.Application.MappingInterface;
+using SCA.Application.ProductDTOs;
+using SCA.Application.ServiceInterface;
+using SCA.Domain.RepositoryInterface;
+
+namespace SCA.Application.ServiceImplementation
+{
+    public class ProductService(IProductRepository productRepository, IProductMapper productMapper) : IProductService
+    {
+        public async Task AddProductAsync(CreateProductDto createProductDto)
+        {
+            var product = productMapper.MapToEntity(createProductDto);
+            await productRepository.AddProductAsync(product);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            await productRepository.DeleteProductAsync(id);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllProductsAsync()
+        {
+            var products = await productRepository.GetAllProductsAsync();
+            return products.Select(product => productMapper.MapToDto(product)).ToList();
+        }
+
+        public async Task<ProductDto?> GetProductByIdAsync(int id)
+        {
+            var product = await productRepository.GetProductByIdAsync(id);
+            return product == null ? null : productMapper.MapToDto(product);
+        }
+
+        public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            var product = productMapper.MapToEntity(updateProductDto);
+            await productRepository.AddProductAsync(product);
+        }
+    }
+}
---
+++ SCA.Application/ServiceInterface/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using SCA.Application.ProductDTOs;
+
+namespace SCA.Application.ServiceInterface
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<ProductDto?> GetProductByIdAsync(int id);
+        Task AddProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(int id);
+    }
+}
