a85ef25188379b48a473e2b535f486bf77db24e2
stefanborcia <stefigabi89@yahoo.com>
2025-04-25 19:35:05 +0200

    Install Swagger and Auto-open browser to Swagger UI

--- SCA.Presentation/Program.cs
+++ SCA.Presentation/Program.cs
@@ -9,14 +9,16 @@ builder.Services.AddInfrastructureService(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
