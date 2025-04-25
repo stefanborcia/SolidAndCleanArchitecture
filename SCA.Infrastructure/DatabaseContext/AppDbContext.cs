a7ecd0e71e665976cacbb92b8759d8a58bcb0ade
stefanborcia <stefigabi89@yahoo.com>
2025-04-25 09:31:14 +0200

    Implement Repository and DbContext in Infrastructure

---
+++ SCA.Infrastructure/DatabaseContext/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SCA.Domain.ProductEntity;
+
+namespace SCA.Infrastructure.DatabaseContext
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SCA.Infrastructure/RepositoryImplementation/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SCA.Domain.ProductEntity;
+using SCA.Domain.RepositoryInterface;
+using SCA.Infrastructure.DatabaseContext;
+
+namespace SCA.Infrastructure.RepositoryImplementation
+{
+    public class ProductRepository(AppDbContext context) : IProductRepository
+    {
+        public async Task AddProductAsync(Product product)
+        {
+                await context.Products.AddAsync(product);
+                await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var products = await context.Products.FindAsync(id);
+
+            if (products != null)
+            {
+                context.Products.Remove(products);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync() => await context.Products.AsNoTracking().ToListAsync();
+
+        public async Task<Product?> GetProductByIdAsync(int id) => await context.Products.FindAsync(id);
+
+        public Task UpdateProductAsync(Product product)
+        {
+            context.Products.Update(product);
+            return context.SaveChangesAsync();
+        }
+    }
+}
